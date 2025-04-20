b46ca005702291b762e546d1c9f0afd5a97b0cd5
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-04-20 06:51:15 -0400

    Merge branch 'baseline' of https://github.com/bahrus/per-each into baseline

---
+++ emc.js
@@ -0,0 +1,22 @@
+// @ts-check
+import { BeHive, seed, MountObserver } from 'be-hive/be-hive.js';
+/** @import {EMC} from './ts-refs/trans-render/be/types' */
+/** @import {Actions, PAP,  AP} from './ts-refs/be-flashy/types' */;
+
+/**
+ * @type {EMC<any, AP>}
+ */
+export const emc = {
+    base: 'per-each',
+    enhPropKey: 'perEach',
+    map: {
+
+    },
+    importEnh: async () => {
+        const { PerEach } = await import('./per-each.js');
+        return PerEach;
+    },
+}
+
+const mose = seed(emc);
+MountObserver.synthesize(document, BeHive, mose); 
